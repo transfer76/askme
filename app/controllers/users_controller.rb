645846134fd44c2a34bb2021147676685f6c2eb1
transfer76 <shina76@ukr.net>
2019-02-19 00:46:58 +0200

    Add created users form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,39 +1,27 @@
 class UsersController < ApplicationController
   def index
-   @users = [
-          User.new(
-            id: 1,
-            name: 'Vova',
-            username: 'transfer',
-            avatar_url: ''
-            ),
-          User.new(
-            id: 2,
-            name: 'Misha',
-            username: 'aristofan'
-          )
-    ] 
+    @users = User.all
   end
 
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.save
   end
 
   def edit
   end
 
   def show
-     @user = User.new(
-       name: 'Vova',
-       username: 'transfer')
+  end
 
-     @questions = [
-       Question.new(text: 'Как дела?', created_at: Date.parse('01.01.0001')),
-       Question.new(text: 'Вчем смысл?', created_at: Date.parse('01.01.0001')),
-       Question.new(text: 'В чем правда, брат?', created_at: Date.parse('01.01.0001')),
-       Question.new(text: 'Почем нынче фунт лиха?', created_at: Date.parse('01.01.0001')),
-       Question.new(text: 'Ты где?', created_at: Date.parse('01.01.0001'))
-     ]
+  private
 
-     @new_question = Question.new
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation,
+                                 :name, :username, :avatar_url)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   root 'users#index'
-  get 'show' => 'users#show'
 
-  resources :users
+  resources :users, except: [:destroy]
   resources :questions
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
