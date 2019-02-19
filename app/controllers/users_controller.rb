2f7c66782a65b4286ef6a02182d381ba517162f5
transfer76 <shina76@ukr.net>
2019-02-19 16:32:55 +0200

    Show errors validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,8 @@ class UsersController < ApplicationController
    
      if @user.save
        redirect_to root_url, notice: 'Пользователь успешно зарегистрирован!'
+     else
+       render 'new'  
      end
    end
 
