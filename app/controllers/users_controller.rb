5b13903a12e496720fc88264e8c4ca75c0f50621
transfer76 <shina76@ukr.net>
2019-02-19 21:42:52 +0200

    Add update user function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,18 @@ class UsersController < ApplicationController
    end
 
   def edit
+    @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: 'Данные обновлены'
+    else
+      render 'edit'
+    end  
+  end  
 
   def show
     @user = User.find(params[:id])
