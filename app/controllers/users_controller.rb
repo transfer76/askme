3249097786c8809ec301c701eba06bc2029df8dd
transfer76 <shina76@ukr.net>
2019-02-23 01:17:48 +0200

    Refactoring user destroy function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :load_user, except: [:index, :create, :new]
-  before_action :authorize_user, except: [:index, :new, :create, :show, :destroy]
+  before_action :authorize_user, except: [:index, :new, :create, :show]
 
   def index
     @users = User.all
