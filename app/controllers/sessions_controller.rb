ac45a5a9da11f23c1d9b960f3b87f55416893b72
transfer76 <shina76@ukr.net>
2019-02-22 14:58:13 +0200

    Change root-path after user registratoin and refactor regexp username validation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
 
     if user.present?
       session[:user_id] = user.id
-      redirect_to root_url, notice: 'Вы успешно залогинились'
+      redirect_to user_path(current_user), notice: 'Вы успешно залогинились'
     else
       flash.now.alert = 'Неправильный мэйл или пароль'
       render :new
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
     redirect_to root_url, alert: 'Вы уже залогинены' if current_user.present?
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_url, notice: 'Пользователь успешно зарегистрирован!'
+      redirect_to user_path(session[:user_id] = @user.id), notice: 'Пользователь успешно зарегистрирован!'
      else
        render 'new'
      end
