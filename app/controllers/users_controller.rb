944826982a0ed1c4ff18ce6326873cc7274d6afd
transfer76 <shina76@ukr.net>
2019-02-22 23:35:57 +0200

    Add function to destroy user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :load_user, except: [:index, :create, :new]
-  before_action :authorize_user, except: [:index, :new, :create, :show]
+  before_action :authorize_user, except: [:index, :new, :create, :show, :destroy]
 
   def index
     @users = User.all
@@ -39,6 +39,12 @@ class UsersController < ApplicationController
     @questions_count = @questions.count
     @answers_count = @questions.where.not(answer: nil).count
     @unanswered_count = @questions_count - @answers_count
+  end
+
+  def destroy
+    @user = current_user
+    @user.destroy
+    redirect_to root_url, notice: 'Пользователь удалён'
   end
 
   private
