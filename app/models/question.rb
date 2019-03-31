19a7837194c493f3987d8f79f80a137c109ec5a9
transfer76 <shina76@ukr.net>
2019-04-01 00:44:27 +0300

    Add association to question model

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,5 +2,8 @@ class Question < ApplicationRecord
   belongs_to :user
   belongs_to :author, class_name: 'User', foreign_key: "author_id", optional: true
 
+  has_many :hashtags_questions
+  has_many :hashtags, through: :hashtags_questions
+
   validates :text, :user, presence: true, length: { maximum: 255 }
 end
