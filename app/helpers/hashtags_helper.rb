bee28b26bb102f9223216108d3af17c76da32f3a
transfer76 <shina76@ukr.net>
2019-04-01 23:36:55 +0300

    Add hashtags helper

---
+++ app/helpers/hashtags_helper.rb
@@ -0,0 +1,10 @@
+module HashtagsHelper
+  def add_hashtag_links(s)
+    result =
+      s.gsub(Hashtag::REGEXP) do |name|
+        link_to name, hashtag_url(name)
+      end
+    
+    result.html_safe
+  end
+end
