36bb89a19283ab802ce433f27141fe3486e7047e
transfer76 <shina76@ukr.net>
2019-04-02 22:55:36 +0300

    Delete # %23 from URL

--- app/helpers/hashtags_helper.rb
+++ app/helpers/hashtags_helper.rb
@@ -2,7 +2,7 @@ module HashtagsHelper
   def add_hashtag_links(s)
     result =
       s.gsub(Hashtag::REGEXP) do |name|
-        link_to name, hashtag_url(name)
+        link_to name, hashtag_url(name.delete("#"))
       end
     
     result.html_safe
--- app/models/question.rb
+++ app/models/question.rb
@@ -15,7 +15,7 @@ class Question < ApplicationRecord
     hashtags_questions.clear
 
     "#{text} #{answer}".scan(Hashtag::REGEXP).uniq.each do |name|
-      hashtags << Hashtag.find_or_create_by!(name: name)
+      hashtags << Hashtag.find_or_create_by!(name: name.delete("#"))
     end
   end
 end
